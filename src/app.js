878b8dcbd943e18c11d636f349105b5205227ea9
Jessica Navarro <jess.nav359.herrera@gmail.com>
2018-07-13 11:43:14 -0400

    codigo para logingoogle

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 var provider = new firebase.auth.FacebookAuthProvider();
+
+$('#logingoogle').click(function(){
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then(function(result){
+    console.log(result.user);
+  });
+});
